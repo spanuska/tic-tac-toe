8b63f57f37596a5b325940b5c4e921faf66a0116
Skylar Panuska <skylar.panuska@gmail.com>
2014-10-13 08:32:17 -0400

    ordered fns() closer to call site, -old code/notes

--- tttJsSkylarPanuska2014.js
+++ tttJsSkylarPanuska2014.js
@@ -43,6 +43,18 @@ function displayFirstAndSecondPlayers(){ //function called on player name submit
 	showPlayer();
 }
 
+//--------Display player order--------//
+function showPlayer(){ //function called in displayFirstAndSecondPlayer() and takeTurn()
+	console.log("showPlayerOrder ran") //test
+	if (activePlayer == playerA){
+		$("#turn").css("background-color", "#97DB97");
+		$("#turn").html(playerA + ", it's your turn! Click on the '?' where you want your 'X' to go.");
+	}else{
+		$("#turn").css("background-color", "#0099CC");
+		$("#turn").text(playerB + ", it's your turn! Click on the '?' where you want your 'O' to go.");
+	}
+}
+
 //--------Mark the game board; function called on player name submit--------//
 function takeTurn(){
 	$(".clickable").bind({
@@ -62,44 +74,26 @@ function takeTurn(){
 			
 			assignActivePlayer();
 			showPlayer();
-
-			$(this).removeClass("clickable").addClass("unclickable"); //the thinking is to use this and the code below (before winConditions()) in order to prohibit and alert user when they can't click a cell that is already occupied and must pick another...but it's not there yet.
-
-			/*$(".unclickable").click(function(event){
-			console.log("they clicked an unclickable cell!");
-			$("#players").text("That spot is not open. Please pick a different (open) spot, " + activePlayer + ".")
-			});*/
 		}
 	});
 }
 
-//------------ID active player & display results--------//
+//------------ID active player--------//
 function assignActivePlayer(){ //function called in takeTurn()
 	activePlayer = (activePlayer != playerA) ? playerA : playerB;
 	
 	console.log("assignActivePlayer ran, and the activePlayer is now " + activePlayer); //test
 }
 
-//--------Display player order--------//
-function showPlayer(){ //function called in takeTurn()
-	console.log("showPlayerOrder ran") //test
-	if (activePlayer == playerA){
-		$("#turn").css("background-color", "#97DB97");
-		$("#turn").html(playerA + ", it's your turn! Click on the '?' where you want your 'X' to go.");
-	}else{
-		$("#turn").css("background-color", "#0099CC");
-		$("#turn").text(playerB + ", it's your turn! Click on the '?' where you want your 'O' to go.");
-	}
+function checkForWinConditions(){
+
 }
-
-
-
 
 
 //-JP's suggestion: get element by id, get text, make function that takes 3 parameters, check if all 3 are the same and not "?"
 
-/*winConditions = {
-	["#a1", "#a2", "#a3"],
+winConditions = {
+	[("#a1" "X", "#a2" = "X", "#a3" = "X"} || ],
 	["#b1", "#b2", "#b3"],
 	["#c1", "#c2", "#c3"],
 	["#a1", "#b1", "#c1"],
@@ -119,30 +113,9 @@ function checkForWin(winConditions){
 //------------Create table object (part of checking for win condition, would be part of takeTurn()...(not working yet!)--------//
 I started trying to generate this table in order to check against the win conditions but couldn't get it to work in time.
 
-/*$("table#theBoard tr").each(function() { 
-boardTable = {};
-var idString = $( this ).attr( "id" );
-cell = $(idString)
-	
-cellID = cell.find("#id"); 
-cellValue = cell.text; 
-
-    if ($(this).length > 0) { 
-        $(this).each(function() { 
-           cell.push(cellID);
-           cell.push(cellValue);
-           });
-        console.log(cellID);
-        console.log(cellValue);
-        console.log(cell);
-        boardTable.add(cell);
-    }
-});	
-}*/
-
 
 /*Next steps:
--Prohibit clicking on a cell that's already been clicked
+X-Prohibit clicking on a cell that's already been clicked (unbind click)
 -Build a way to store board so that after each turn the program can...
 -Compare the state of the board to win conditions in order to...
 -(May need to modify winConditions() as written, depending on how comparison works)
